7bf42089199a91b36dfef09e09b0c0eeb23c1e27
J. Ryan Conklin <jryanconklin@gmail.com>
2016-06-17 10:52:23 -0700

    Put attribute generator inside a function, and use the function to fill the 4d6 array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
+var attributeArray = [];
 var total = 0;
+var numbers = [];
+var dieCount = 0;
 
-var d6_1 = 1 + Math.floor(Math.random() * 6);
-var d6_2 = 1 + Math.floor(Math.random() * 6);
-var d6_3 = 1 + Math.floor(Math.random() * 6);
-var d6_4 = 1 + Math.floor(Math.random() * 6);
-var numbers = [d6_1, d6_2, d6_3, d6_4];
+function attributeGen() {
+	return 1 + Math.floor(Math.random() * 6);
+}
+
+while ( dieCount < 4 ) {
+	numbers.push(attributeGen());
+	dieCount += 1;
+}
 
 numbers.sort();
-alert(numbers);
 numbers.shift();
-alert(numbers);
 
 numbers.forEach(function(number) {
-	total += number
+	total += number;
 });
 alert(total);
+console.log(total);
+
+attributeArray.push(total);
+console.log(attributeArray);
+
+// do {
+//
+// } while (attributeArray.length < 6 );
+// console.log(attributeArray);
